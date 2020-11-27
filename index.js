81e86f3a64cdd7f3a6a5898caa9f9644fe70a9fe
ryancdavey1 <ryancdavey1@gmail.com>
2020-11-27 11:20:10 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  let result = arr.find( record => record.result === "W" )
+  return !!result ? result.year : undefined
+}
